30ee502771bcd6f4da7768c2664df0f50a887852
liuxuandong <liuxuandong117@163.com>
2019-10-27 13:22:34 +0800

    Project Init

---
+++ src/main/java/com/pacificareps/kayla/KaylaApplication.java
@@ -0,0 +1,13 @@
+package com.pacificareps.kayla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaylaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaylaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pacificareps/kayla/KaylaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacificareps.kayla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaylaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
